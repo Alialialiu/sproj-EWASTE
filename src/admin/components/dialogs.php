3c2e851b002db83db0a497a2cb2797b5b8286027
alilulu <104284214+Alialialiu@users.noreply.github.com>
2024-03-29 15:46:46 +0800

    v1.0.0
    v1.0.0

---
+++ src/admin/components/dialogs.php
@@ -0,0 +1,15 @@
+ <dialog id="ctyAddModal" class="modal">
+     <div class="modal-box">
+         <form method="dialog">
+             <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+         </form>
+         <h3 class="font-bold text-lg">Add Category</h3>
+         <form class="mt-2" action="" id="ctyform">
+             <label class="input input-bordered flex items-center gap-2">
+                 Category Name:
+                 <input type="text" name="ctyname" class="grow" placeholder="text input" />
+             </label>
+             <button type="submit" class="btn btn-primary btn-sm mt-2">add</button>
+         </form>
+     </div>
+ </dialog>
---
+++ src/admin/components/navigation.php
@@ -0,0 +1,13 @@
+<div class="lg:w-[18%] h-screen rounded-r-[20px] hidden bg-[#D6CC99] lg:flex flex-col p-2">
+    <a href="" class="btn btn-ghost text-xl text-center mt-3 text-white">E-WASTE</a>
+    <ul class="flex flex-col mt-9">
+        <label class="input input-bordered input-sm flex items-center gap-2">
+            <input type="text" class="grow" placeholder="Search" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70">
+                <path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" />
+            </svg>
+        </label>
+        <a href="index.php" id="btncstm1" class="btn mt-2 ">Home</a>
+        <a href="category.php" id="btncstm2" class="btn mt-2 btn-active btn-neutral">Categories</a>
+    </ul>
+</div>
